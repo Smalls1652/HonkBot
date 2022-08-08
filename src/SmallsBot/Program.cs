aed615c37b49a05aee6cff0aeab5abef2f925217
Timothy Small <smalls.online@icloud.com>
2022-08-08 17:59:28 -0400

    Updated Program
    Will now build and run a host

--- src/SmallsBot/Program.cs
+++ src/SmallsBot/Program.cs
@@ -1,9 +1,63 @@
-﻿namespace SmallsBot;
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SmallsBot.Models.Services;
+
+namespace SmallsBot;
 
 public class Program
 {
-    public async Task Main()
+
+    public static async Task Main()
     {
-        Console.WriteLine("Hello world!");
+        IHostBuilder hostBuilder = Host.CreateDefaultBuilder();
+
+        hostBuilder
+            .ConfigureServices(
+                (_, services) =>
+                {
+                    services.AddSingleton<IDiscordService, DiscordService>();
+                }
+            )
+            .ConfigureLogging(
+                (loggerOptions) =>
+                {
+                    loggerOptions.AddConsole();
+                }
+            );
+
+
+        hostBuilder.ConfigureAppConfiguration(
+            (hostingContext, config) =>
+            {
+                config.Sources.Clear();
+
+                IHostEnvironment hostEnv = hostingContext.HostingEnvironment;
+
+                config
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile(
+                        path: "appsettings.json",
+                        optional: true,
+                        reloadOnChange: true
+                    )
+                    .AddJsonFile(
+                        path: $"appsettings.{hostEnv.EnvironmentName}.json",
+                        optional: true,
+                        reloadOnChange: true
+                    );
+
+                config.AddEnvironmentVariables();
+            }
+        );
+
+        using IHost host = hostBuilder.Build();
+
+        await host.Services.GetRequiredService<IDiscordService>().Connect();
+
+        await host.RunAsync();
     }
 }
