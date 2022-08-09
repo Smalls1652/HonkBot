2bc457cb0b85a4fd2017c5d0ce14d6ca33f7e9eb
Timothy Small <smalls.online@icloud.com>
2022-08-08 23:03:39 -0400

    Added HonkCommandModule

---
+++ src/SmallsBot/commands/HonkCommandModule.cs
@@ -0,0 +1,38 @@
+using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Microsoft.Extensions.Logging;
+using SmallsBot.Models.Services;
+
+namespace SmallsBot.Commands;
+
+public class HonkCommandModule : InteractionModuleBase
+{
+    private readonly ILogger<HonkCommandModule> _logger;
+    public HonkCommandModule(ILogger<HonkCommandModule> logger)
+    {
+        _logger = logger;
+    }
+
+    [SlashCommand(name: "honk-gm", description: "honk says gm to a user")]
+    public async Task HandleHonkGmSlashCommand(IUser user)
+    {
+        _logger.LogInformation("'{Username}' called the 'honk-gm' command.", Context.User.Username);
+        await RespondWithFileAsync(
+            text: $"gm {user.Mention}",
+            allowedMentions: AllowedMentions.All,
+            filePath: "honk.png"
+        );
+    }
+
+    [SlashCommand(name: "honk-angry", description:"honk mad")]
+    public async Task HandleHonkAngryResponseCommand(string message)
+    {
+        _logger.LogInformation("'{Username}' called the 'honk-angry' command.", Context.User.Username);
+        await RespondWithFileAsync(
+            text: message,
+            filePath: "honk-gon-get-ya.png"
+        );
+    }
+}
