e159f09b7bde895547ddb2dd16ad996e24f50599
Timothy Small <smalls.online@icloud.com>
2022-08-23 12:16:41 -0400

    Renamed ResetHonkStatus to SetHonkStatus

--- src/HonkBot/modules/HonkCommandModule/commands/HandleSetHonkStatus.cs
+++ src/HonkBot/modules/HonkCommandModule/commands/HandleSetHonkStatus.cs
@@ -12,8 +12,8 @@ public partial class HonkCommandModule : InteractionModuleBase
     /// <param name="status">The status to set.</param>
     /// <param name="activityType">The type of activity to show.</param>
     [DefaultMemberPermissions(GuildPermission.Administrator)]
-    [SlashCommand("reset-honk-status", "Reset honk's status")]
-    public async Task HandleResetHonkStatus(
+    [SlashCommand("set-honk-status", "Set honk's status")]
+    public async Task HandleSetHonkStatus(
         [Summary(description: "The status you want to set for HonkBot.")]
         string status = "honking away",
         [Summary(description: "The type of activity you want to show."), Choice("Playing", 0), Choice("Streaming", 1), Choice("Listening", 2), Choice("Watching", 3), Choice("Competing", 5)]
