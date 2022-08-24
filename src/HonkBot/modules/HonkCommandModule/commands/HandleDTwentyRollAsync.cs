1eeb9d5fd0101d84bf264ca7feb6b273ceb2c297
Timothy Small <smalls.online@icloud.com>
2022-08-24 19:36:32 -0400

    Initial commit of adding more to the d20 rolls

--- src/HonkBot/modules/HonkCommandModule/commands/HandleDTwentyRollAsync.cs
+++ src/HonkBot/modules/HonkCommandModule/commands/HandleDTwentyRollAsync.cs
@@ -11,22 +11,49 @@ public partial class HonkCommandModule : InteractionModuleBase
     /// HonkBot rolls a D20.
     /// </summary>
     [SlashCommand(name: "roll-d20", description: "Roll a D20 dice!")]
-    private async Task HandleDTwentyRollAsync()
+    private async Task HandleDTwentyRollAsync(
+        [Summary(name: "roll-for", description: "What you want to roll for?")]
+        string rollFor
+    )
     {
         await DeferAsync();
 
-        int d20RollVal = RandomGenerator.GetRandomNumber(
+        int userD20RollVal = RandomGenerator.GetRandomNumber(
             minValue: 1,
             maxValue: 20
         );
 
-        string outputMessage = d20RollVal switch
+        int dmD20RollVal = RandomGenerator.GetRandomNumber(
+            minValue: 1,
+            maxValue: 20
+        );
+
+        bool passesCheck;
+        if (userD20RollVal == 1)
         {
-            1 => "Wow! You rolled a natural 1! Sucks for you!",
-            20 => "Wow! You rolled a natural 20! You're either lucky or you're cheating!",
-            _ => $"You rolled a {d20RollVal}."
+            passesCheck = false;
+        }
+        else if (userD20RollVal == 20)
+        {
+            passesCheck = true;
+        }
+        else
+        {
+            passesCheck = userD20RollVal > dmD20RollVal;
+        }
+
+        string passCheckMessage = passesCheck switch
+        {
+            true => "**Check result:**\n```diff\n-Failed\n```",
+            _ => "**Check result:**\n```diff\n+Passed\n```"
         };
 
+        string outputMessage = userD20RollVal switch
+        {
+            1 => $"Wow! {Context.User.Mention} rolled a natural `1` for '**{rollFor}**'! That sucks! lmao\n\n{passCheckMessage}",
+            20 => $"Wow! {Context.User.Mention} rolled a natural `20` for '**{rollFor}**'! {Context.User.Mention} is either lucky or cheating!\n\n{passCheckMessage}",
+            _ => $"{Context.User.Mention} rolled `{userD20RollVal}` for '**{rollFor}**'.\n\n{passCheckMessage}"
+        };
         await FollowupAsync(
             text: outputMessage
         );
