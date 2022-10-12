561460cc85f2aa4a3115fcf88a15a7896051317c
Timothy Small <smalls.online@icloud.com>
2022-10-11 21:15:24 -0400

    Modified how Hugemoji handles emojis
    /hugemoji will now properly handle native Unicode emojis by throwing
    an error back to the user. There's really no need to have HonkBot
    try and handle those because... It just does what would happen if the
    user just typed in the emoji by itself anyway.

--- src/HonkBot/modules/GenericCommandsModule/commands/HandleHonkingHugemoji.cs
+++ src/HonkBot/modules/GenericCommandsModule/commands/HandleHonkingHugemoji.cs
@@ -20,11 +20,28 @@ public partial class ImageCommandsModule : InteractionModuleBase
     )
     {
         // Parse the emote passed through.
-        Emote? parsedEmote;
+        Emote? parsedEmote = null;
         try
         {
             _logger.LogInformation("Parsing '{emote}'.", emote);
-            parsedEmote = Emote.Parse(emote);
+            try
+            {
+                parsedEmote = Emote.Parse(emote);
+            }
+            catch
+            {
+                Emoji parsedEmoji = new(emote);
+
+                if (parsedEmoji is not null)
+                {
+                    await RespondAsync(
+                        text: "Emojis aren't supported for making honking huge. 🤔🤔🤔",
+                        ephemeral: true
+                    );
+
+                    return;
+                }
+            }
 
             // Throw an exception if the parse returned null.
             if (parsedEmote is null)
@@ -37,7 +54,7 @@ public partial class ImageCommandsModule : InteractionModuleBase
             // Throw an error back to the client that the emote failed to parse.
             _logger.LogInformation("Failed to parse the emote.");
             await RespondAsync(
-                text: "Failed to parse emote. >:(",
+                text: "Failed to parse emote. 🤬",
                 ephemeral: true
             );
 
@@ -46,54 +63,57 @@ public partial class ImageCommandsModule : InteractionModuleBase
 
         await DeferAsync();
 
-        try
+        if (parsedEmote is not null)
         {
-            // Download the emote's image file.
-            _logger.LogInformation("Attempting to get emote from '{Url}'.", parsedEmote.Url);
-            Stream? emoteImgStream = await DownloadEmoteAsync(parsedEmote);
-
-            if (emoteImgStream is null)
+            try
             {
-                // Throw an error if emote image stream is null.
-                throw new Exception("Emote image has a null file stream.");
-            }
-            else
-            {
-                // If the image stream wasn't null, start the resize process.
-                _logger.LogInformation("Attempting to resize emote.");
+                // Download the emote's image file.
+                _logger.LogInformation("Attempting to get emote from '{Url}'.", parsedEmote.Url);
+                Stream? emoteImgStream = await DownloadEmoteAsync(parsedEmote);
 
-                // Get the image's info and resize it.
-                MagickImageInfo emoteImgInfo = await GetEmoteImageInfoAsync(emoteImgStream);
-                MemoryStream emoteImgResizedStream = ResizeEmote(emoteImgStream, emoteImgInfo);
-
-                // Send the resized emote to the client.
-                _logger.LogInformation("Sending resized emote.");
-                await FollowupWithFileAsync(
-                    fileName: $"{parsedEmote.Name}.{emoteImgInfo.Format.ToString().ToLower()}",
-                    fileStream: emoteImgResizedStream
-                );
-
-                await emoteImgStream.DisposeAsync();
-            }
-        }
-        catch (Exception e)
-        {
-            // If any error occurred while resizing the emote,
-            // send the base emote image back to the client.
-
-            _logger.LogInformation("Failed to resize the emote. Falling back to sending the raw emote image.");
-
-            _logger.LogError(
-                exception: e,
-                message: "Error message: {Message}",
-                args: e.Message
-            );
-            await FollowupAsync(
-                embed: new EmbedBuilder()
+                if (emoteImgStream is null)
                 {
-                    ImageUrl = Emote.Parse(emote).Url
-                }.Build()
-            );
+                    // Throw an error if emote image stream is null.
+                    throw new Exception("Emote image has a null file stream.");
+                }
+                else
+                {
+                    // If the image stream wasn't null, start the resize process.
+                    _logger.LogInformation("Attempting to resize emote.");
+
+                    // Get the image's info and resize it.
+                    MagickImageInfo emoteImgInfo = await GetEmoteImageInfoAsync(emoteImgStream);
+                    MemoryStream emoteImgResizedStream = ResizeEmote(emoteImgStream, emoteImgInfo);
+
+                    // Send the resized emote to the client.
+                    _logger.LogInformation("Sending resized emote.");
+                    await FollowupWithFileAsync(
+                        fileName: $"{parsedEmote.Name}.{emoteImgInfo.Format.ToString().ToLower()}",
+                        fileStream: emoteImgResizedStream
+                    );
+
+                    await emoteImgStream.DisposeAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                // If any error occurred while resizing the emote,
+                // send the base emote image back to the client.
+
+                _logger.LogInformation("Failed to resize the emote. Falling back to sending the raw emote image.");
+
+                _logger.LogError(
+                    exception: e,
+                    message: "Error message: {Message}",
+                    args: e.Message
+                );
+                await FollowupAsync(
+                    embed: new EmbedBuilder()
+                    {
+                        ImageUrl = Emote.Parse(emote).Url
+                    }.Build()
+                );
+            }
         }
     }
 }
