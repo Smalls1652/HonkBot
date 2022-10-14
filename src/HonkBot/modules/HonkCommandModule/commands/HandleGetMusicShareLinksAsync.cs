cfa0681a48ddfecdf316f7269cd55f8beb26187e
Timothy Small <smalls.online@icloud.com>
2022-10-14 10:53:27 -0400

    Added handling for null platform values
    Whenever HonkBot is unable to get a platform specific share url for a
    song on YouTube, Spotify, or Apple Music, it will now disable the button
    for that platform. This should fix the issue with HonkBot getting stuck
    on "HonkBot is thinking..." when it can't find a share url for that
    platform.

--- src/HonkBot/modules/HonkCommandModule/commands/HandleGetMusicShareLinksAsync.cs
+++ src/HonkBot/modules/HonkCommandModule/commands/HandleGetMusicShareLinksAsync.cs
@@ -23,10 +23,53 @@ public partial class HonkCommandModule : InteractionModuleBase
 
         MusicEntityItem musicEntityItem = await _odesliService.GetShareLinksAsync(musicShareUrl);
 
-        PlatformEntityLink youtubeLink = musicEntityItem.LinksByPlatform!["youtube"];
-        PlatformEntityLink itunesLink = musicEntityItem.LinksByPlatform!["itunes"];
-        PlatformEntityLink appleMusicLink = musicEntityItem.LinksByPlatform!["appleMusic"];
-        PlatformEntityLink spotifyLink = musicEntityItem.LinksByPlatform!["spotify"];
+        PlatformEntityLink? itunesLink;
+        try
+        {
+            itunesLink = musicEntityItem.LinksByPlatform!["itunes"];
+        }
+        catch
+        {
+            _logger.LogError("No iTunes link found for {musicShareUrl}", musicShareUrl);
+            await FollowupAsync(
+                text: "I was unable to get the necessary information from Odesli. :(",
+                ephemeral: true
+            );
+            return;
+        }
+
+        PlatformEntityLink? youtubeLink;
+        try
+        {
+            youtubeLink = musicEntityItem.LinksByPlatform!["youtube"];
+        }
+        catch
+        {
+            _logger.LogWarning("No YouTube link found for {musicShareUrl}", musicShareUrl);
+            youtubeLink = null;
+        }
+
+        PlatformEntityLink? appleMusicLink;
+        try
+        {
+            appleMusicLink = musicEntityItem.LinksByPlatform!["appleMusic"];
+        }
+        catch
+        {
+            _logger.LogWarning("No Apple Music link found for {musicShareUrl}", musicShareUrl);
+            appleMusicLink = null;
+        }
+
+        PlatformEntityLink? spotifyLink;
+        try
+        {
+            spotifyLink = musicEntityItem.LinksByPlatform!["spotify"];
+        }
+        catch
+        {
+            _logger.LogWarning("No Spotify link found for {musicShareUrl}", musicShareUrl);
+            spotifyLink = null;
+        }
 
         StreamingEntityItem itunes = musicEntityItem.EntitiesByUniqueId![itunesLink.EntityUniqueId!];
 
@@ -34,23 +77,62 @@ public partial class HonkCommandModule : InteractionModuleBase
         HttpResponseMessage responseMessage = await httpClient.GetAsync(itunes.ThumbnailUrl);
         Stream imageStream = await responseMessage.Content.ReadAsStreamAsync();
 
-        ButtonBuilder youtubeButton = new(
-            label: "YouTube",
-            style: ButtonStyle.Link,
-            url: youtubeLink.Url!.ToString()
-        );
+        ButtonBuilder youtubeButton;
+        if (youtubeLink is not null)
+        {
+            youtubeButton = new(
+                label: "YouTube",
+                style: ButtonStyle.Link,
+                url: youtubeLink.Url!.ToString()
+            );
+        }
+        else
+        {
+            youtubeButton = new(
+                label: "YouTube 🚫",
+                style: ButtonStyle.Secondary,
+                isDisabled: true,
+                customId: $"{musicEntityItem.EntityUniqueId}-youtube-disabled"
+            );
+        }
 
-        ButtonBuilder appleMusicButton = new(
-            label: "Apple Music",
-            style: ButtonStyle.Link,
-            url: appleMusicLink.Url!.ToString()
-        );
+        ButtonBuilder appleMusicButton;
+        if (appleMusicLink is not null)
+        {
+            appleMusicButton = new(
+                label: "Apple Music",
+                style: ButtonStyle.Link,
+                url: appleMusicLink.Url!.ToString()
+            );
+        }
+        else
+        {
+            appleMusicButton = new(
+                label: "Apple Music 🚫",
+                style: ButtonStyle.Secondary,
+                isDisabled: true,
+                customId: $"{musicEntityItem.EntityUniqueId}-appleMusic-disabled"
+            );
+        }
 
-        ButtonBuilder spotifyButton = new(
-            label: "Spotify",
-            style: ButtonStyle.Link,
-            url: spotifyLink.Url!.ToString()
-        );
+        ButtonBuilder spotifyButton;
+        if (spotifyLink is not null)
+        {
+            spotifyButton = new(
+                label: "Spotify",
+                style: ButtonStyle.Link,
+                url: spotifyLink.Url!.ToString()
+            );
+        }
+        else
+        {
+            spotifyButton = new(
+                label: "Spotify 🚫",
+                style: ButtonStyle.Secondary,
+                isDisabled: true,
+                customId: $"{musicEntityItem.EntityUniqueId}-spotify-disabled"
+            );
+        }
 
         ButtonBuilder moreLinksButton = new(
             label: "More links",
